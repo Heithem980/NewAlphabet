aed8f4201094df19605f7c5d0e6a035c59e59608
Heithem980 <heithem.gha98@gmail.com>
2024-05-27 17:47:57 +0200

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -43,7 +43,7 @@
         // Create an instance 
         AlphabetTranslator translator = new AlphabetTranslator(translationMap);
 
-
+        //we
         
         // Text to translate
         string originalText = "Hello World!";
